df85d02f2bee60db46de8ebe26bc9256f3de1da5
aisk <aisk1988@gmail.com>
2013-09-08 14:14:22 +0800

    受伤后的无敌时间

--- app/scripts/components/player.js
+++ app/scripts/components/player.js
@@ -62,15 +62,16 @@
       }
     },
 
-    hurt: function(damage) {
+    hurt: _.throttle(function(damage) {this._hurt(damage);}, 1000),
+
+    _hurt: function(damage) {
       this.hp -= damage;
+        Crafty.audio.play("hurt");
       Crafty.trigger('HurtPlayer', this);
       if (this.hp <= 0) {
         Crafty.audio.play("ah");
         Crafty.trigger('KillPlayer', this);
         return this.destroy();
-      } else {
-        Crafty.audio.play("hurt");
       }
     },
 
